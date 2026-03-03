06714f268b671c59208c6ce1c797dd6423f8fd30
Shalusingh17 <reactshalu@gmail.com>
2026-03-03 08:54:07 +0530

    Auto update from VS Code work

--- automation/linkedin_post.py
+++ automation/linkedin_post.py
@@ -3,14 +3,11 @@ import urllib.parse
 
 def post_to_linkedin(caption, image_path):
 
-    repo_link = "https://github.com/shalusingh17/dev-workflow-automation"
-
-    text = caption + "\n\n" + repo_link
-
+    text = caption
     encoded = urllib.parse.quote(text)
 
     linkedin_url = f"https://www.linkedin.com/feed/?shareActive=true&text={encoded}"
 
-    print("Opening LinkedIn post window")
-
     webbrowser.open(linkedin_url)
+
+    print("LinkedIn opened. Attach this image manually:", image_path)
